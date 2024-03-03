91812a61acc319809ea9f1aac0b4fc031d3df993
Ronlivgit <ron.livne58@gmail.com>
2024-03-03 15:56:48 +0200

    first commit

---
+++ BackEnd/config/index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+//! IN CONFIG FOLDER USE INDEX.JS, OTHERWISE, USE REQUIRE './config/xName' .
+
+const {MONGO_URL,jwtSecret} = process.env
+
+const config = {
+    MONGO_URL,
+    jwtSecret
+}
+
+console.log(config);
+
+module.exports = {config}
