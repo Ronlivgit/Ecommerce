def93760664d825b2cf6e291979688722b8049d3
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-03-04 12:54:23 +0200

    try to back

--- BackEnd/controllers/order.controller.js
+++ BackEnd/controllers/order.controller.js
@@ -1,29 +1,8 @@
-const { Order } = require("../models/order.model")
-const { generateToken, verifyToken } = require("../utils/jwt")
-
-const getorder = async (req, res) => {
-    try {
-        const query = req.query
-        const orderes = await Order.find({ ...query })
-        return res.send({ orderes })
-    } catch (err) {
-        console.log(err);
-        return res.status(404).send(err)
-    }
-}
-const createOrder = async (req, res) => {
-    const body = req.body;
-    try {
-        body.userId = req.body.userId;
-        const newOrder = new Order(body)
-        await newOrder.save()
-        return res.status(201).send(newOrder)
-    } catch (err) {
-        console.log(err);
-        return res.status(404).send(err)
-    }
-}
+const {Order} = require("../models/order.model")
+const {generateToken , verifyToken} = require("../utils/jwt")
 
 
 
-module.exports = { getorder, createOrder }
+
+
+module.exports = {}
