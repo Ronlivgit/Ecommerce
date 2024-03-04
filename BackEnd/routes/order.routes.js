8abc14b1b17fefc3da48625a0492d8758732c5e6
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-03-04 12:00:32 +0200

    ordermodel start

--- BackEnd/routes/order.routes.js
+++ BackEnd/routes/order.routes.js
@@ -1,9 +1,10 @@
-const {Router} = require("express")
+const { Router } = require("express")
 const router = Router()
-// const {Authentication} = require("../middleware/authentication")
-const {Order} = require("../models/order.model")
+const { Authentication } = require("../middleware/authentication")
+const { Order } = require("../models/order.model")
+const { createOrder, getorder } = require('../controllers/order.controller')
 
-
+router.get('/', Authentication, getorder)
 
 
 module.exports = router
