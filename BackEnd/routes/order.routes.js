bd322761e08f5729b26dccc7cd811e458a0e5199
Ronlivgit <ron.livne58@gmail.com>
2024-03-04 14:36:15 +0200

    a

--- BackEnd/routes/order.routes.js
+++ BackEnd/routes/order.routes.js
@@ -2,7 +2,13 @@ const {Router} = require("express")
 const router = Router()
 // const {Authentication} = require("../middleware/authentication")
 const {Order} = require("../models/order.model")
+const { Authentication } = require("../middleware/authentication")
+const { createOrder, getOrder } = require('../controllers/order.controller')
 
+router.get('/', Authentication, getOrder)
+
+
+module.exports = router
 
 
 
