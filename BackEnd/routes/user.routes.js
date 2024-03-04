7771865cf1204a5307a58f2fc86abde4645d6aeb
Ronlivgit <ron.livne58@gmail.com>
2024-03-04 11:46:30 +0200

    models and controllers+routes done

--- BackEnd/routes/user.routes.js
+++ BackEnd/routes/user.routes.js
@@ -1,8 +1,13 @@
 const {Router} = require("express")
 const router = Router()
 // const {Authentication} = require("../middleware/authentication")
-const {User} = require("../models/user.model")
+const {User} = require('../models/user.model')
+const { registerUser, logUser } = require("../controllers/user.controller")
 
+
+router.post('/register' , registerUser)
+
+router.post('/login' , logUser)
 
 
 
