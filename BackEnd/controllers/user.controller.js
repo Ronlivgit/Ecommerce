74241aa25aa533a99e5fe3298ccd1806d8674e59
Ronlivgit <ron.livne58@gmail.com>
2024-03-06 10:28:15 +0200

    check

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,10 +1,27 @@
-const { User } = require("../models/user.model")
+const { User } = require("../models/checkIfExist.model")
 const { generateToken, verifyToken } = require("../utils/jwt")
 const bcrypt = require("bcryptjs")
 
-const getusers = async (req, res) => {
-    console.log('got here');
-}
+const getUsers = async (req, res) => {
+    try {
+        const query = req.query
+        const Users = await User.find({ ...query });
+        return res.send(Users);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send("error");
+    }}
+
+    const getAuth = async (req, res) =>{
+        const checkIfExist = req.checkIfExist;
+        try {
+            const dbUser = await User.findById(checkIfExist.id);
+            res.send(dbUser);
+        } catch (error) {
+            console.log(error);
+            res.status(400).send("error");
+        }
+    }
 
 const registerUser = async (req, res) => {
     const body = req.body
@@ -21,25 +38,62 @@ const registerUser = async (req, res) => {
 }
 
 
+
 const logUser = async (req, res) => {
-    console.log("got here");
     const { email, password, fullName, role } = req.body
     try {
-        const checkIfExist = await User.findOne({ email })
-        if (checkIfExist) {
-            const isMatching = await bcrypt.compare(password, checkIfExist.password)
-            if (isMatching) {
-                console.log("User logged in : ", checkIfExist.userId, checkIfExist.email);
-                const token = generateToken({ ...checkIfExist.email, ...checkIfExist.userId })
+        const checkIfExist = await User.findOne({email })
+        if(checkIfExist){
+            const isMatch = await bcrypt.compare(password, checkIfExist.password);
+            if (isMatch) {
+                const token = generateToken({id: checkIfExist._id, email: checkIfExist.email, role:"admin"})
                 return res.status(200).send({ message: "Logged successfully : ", checkIfExist, token })
+            }else {
+            return res.status(401).json("User not found or invalid credentials");
             }
+        };
+        return res.status(401).json("User not found or invalid credentials");
+    } catch (error) {
+        console.log(error);
+        res.status(400).json("error");
+    }
+}
+
+const updatedUser = async (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    try {
+        let foundUser = await User.findById(id);
+        if (foundUser) {
+            foundUser = await User.findByIdAndUpdate(id, body, { new: true });
+            return res.send({ message: 'User updated successfully', data: foundUser });
+        } else {
+            return res.status(404).send('No user found');
         }
     } catch (error) {
-        console.error(error);
+        console.log(error);
+        res.status(400).send("Error");
     }
 }
 
 
 
+const deleteUser = async (req, res) =>{
+    const { id }= req.params;
+    try {
+        const isDelleted = await User.findByIdAndDelete(id);
+        if(isDelleted){
+           return res.send({messege: 'user deleted'});
+        }
+        return res.send({messege: 'user Not found'});
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("error");
+    }
+}
 
-module.exports = { registerUser, logUser, getusers }
+//! Validate through localStorage token (if checkIfExist has valid token, auto log-in)
+
+
+
+module.exports = { registerUser, logUser, getUsers ,getAuth,updatedUser,deleteUser}
--- FrontEnd/src/pages/about/About.jsx
+++ FrontEnd/src/pages/about/About.jsx
@@ -1,40 +1,20 @@
 
-
-
 import React from "react";
-import { Box, Typography, Button, Paper, styled } from "@mui/material";
-import { Star } from "@mui/icons-material";
-import Slider from "react-slick";
+// import { Box, Typography, Button, Paper, styled } from "@mui/material";
+// import { Star } from "@mui/icons-material";
+// import Slider from "react-slick";
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import myImage from "../components/imgaes/download.jpeg";
-import myImage1 from "../components/imgaes/download (1).jpeg";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
+// import myImage from "../components/imgaes/download.jpeg";
+// import myImage1 from "../components/imgaes/download (1).jpeg";
 
-import myImage4 from "../components/imgaes/img.jpeg";
-import imgg from "../components/imgaes/imgg.jpeg";
-import dwin from "../components/imgaes/shvar.jpeg";
-import silver from "../components/imgaes/silver.jpeg";
-
-
+// // import myImage4 from "../components/imgaes/img.jpeg";
+// import imgg from "../components/imgaes/imgg.jpeg";
+// import dwin from "../components/imgaes/shvar.jpeg";
+// import silver from "../components/imgaes/silver.jpeg";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import React from 'react'
 
 export default function About() {
   return (
