6b4a00103b34464be739afef9a653d87abf7d5c0
nitzanbs <nitzanbs2248@gmail.com>
2024-03-06 12:05:19 +0200

    auth works

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/checkIfExist.model")
+const { User } = require("../models/user.model")
 const { generateToken, verifyToken } = require("../utils/jwt")
 const bcrypt = require("bcryptjs")
 
@@ -10,18 +10,19 @@ const getUsers = async (req, res) => {
     } catch (error) {
         console.log(error);
         return res.status(400).send("error");
-    }}
-
-    const getAuth = async (req, res) =>{
-        const checkIfExist = req.checkIfExist;
-        try {
-            const dbUser = await User.findById(checkIfExist.id);
-            res.send(dbUser);
-        } catch (error) {
-            console.log(error);
-            res.status(400).send("error");
-        }
     }
+}
+
+const getAuth = async (req, res) => {
+    const checkIfExist = req.user;
+    try {
+        const dbUser = await User.findById(checkIfExist.id);
+        res.send(dbUser);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("error");
+    }
+}
 
 const registerUser = async (req, res) => {
     const body = req.body
@@ -42,14 +43,14 @@ const registerUser = async (req, res) => {
 const logUser = async (req, res) => {
     const { email, password, fullName, role } = req.body
     try {
-        const checkIfExist = await User.findOne({email })
-        if(checkIfExist){
+        const checkIfExist = await User.findOne({ email })
+        if (checkIfExist) {
             const isMatch = await bcrypt.compare(password, checkIfExist.password);
             if (isMatch) {
-                const token = generateToken({id: checkIfExist._id, email: checkIfExist.email, role:"admin"})
-                return res.status(200).send({ message: "Logged successfully : ", checkIfExist, token })
-            }else {
-            return res.status(401).json("User not found or invalid credentials");
+                const token = generateToken({ id: checkIfExist._id, email: checkIfExist.email, role: "admin" })
+                return res.status(200).send({ message: "Logged successfully : ", user:checkIfExist, token })
+            } else {
+                return res.status(401).json("User not found or invalid credentials");
             }
         };
         return res.status(401).json("User not found or invalid credentials");
@@ -78,14 +79,14 @@ const updatedUser = async (req, res) => {
 
 
 
-const deleteUser = async (req, res) =>{
-    const { id }= req.params;
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
     try {
         const isDelleted = await User.findByIdAndDelete(id);
-        if(isDelleted){
-           return res.send({messege: 'user deleted'});
+        if (isDelleted) {
+            return res.send({ messege: 'user deleted' });
         }
-        return res.send({messege: 'user Not found'});
+        return res.send({ messege: 'user Not found' });
     } catch (error) {
         console.log(error);
         res.status(400).send("error");
@@ -96,4 +97,4 @@ const deleteUser = async (req, res) =>{
 
 
 
-module.exports = { registerUser, logUser, getUsers ,getAuth,updatedUser,deleteUser}
+module.exports = { registerUser, logUser, getUsers, getAuth, updatedUser, deleteUser }
