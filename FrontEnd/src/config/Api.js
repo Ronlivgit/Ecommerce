967117ca20d7fed16ed79e146850f317fc9ee783
Ronlivgit <ron.livne58@gmail.com>
2024-03-06 10:28:15 +0200

    check

---
+++ FrontEnd/src/config/Api.js
@@ -0,0 +1,4 @@
+export const APIBaseUrl="http://localhost:3000/api"
+
+
+export default {APIBaseUrl}
--- FrontEnd/src/pages/profile/Profile.jsx
+++ FrontEnd/src/pages/profile/Profile.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import SignIn from '../../components/auth/SignIn'
+import { Link } from 'react-router-dom'
+import { UserContext } from '../../context/UserProvider';
 
 export default function Profile() {
+  const { user } = useContext(UserContext);
   return (
-    <div>Profile</div>
+    <>
+      <div>Profile</div>
+      {user ? (
+        <h1>hello {user.fullName}</h1>
+      ) : (
+        <>
+        <h1>pleas Sign in to see profile</h1>
+        <Link to='/signIn'></Link>
+        </>
+
+      )}
+    </>
   )
 }
