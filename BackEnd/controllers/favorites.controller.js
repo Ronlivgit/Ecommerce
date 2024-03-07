ee36b84934db5f40dad41ec6dfdb3ffbc3f080cf
DocDavid1 <meyer2615@gmail.com>
2024-03-07 10:07:12 +0200

    david

--- BackEnd/controllers/favorites.controller.js
+++ BackEnd/controllers/favorites.controller.js
@@ -1,8 +1,60 @@
 const {Favorite} = require("../models/favorites.model")
-const {generateToken , verifyToken} = require("../utils/jwt")
+// const {generateToken , verifyToken} = require("../utils/jwt")
 
 
-//! GET favorites, POST favorites, UPDATE favorites , DELETE favorites
 
 
-module.exports = {}
+
+const getFavorite = async (req, res) => {
+    try {
+        const query = req.query
+        const favorite = await Favorite.find({ ...query });
+        res.send(favorite);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("error");
+    }
+}
+
+
+
+
+const addFavorite = async (req, res) => {
+    try {
+        const body = req.body;
+        const newProduct = new Favorite(body);
+        console.log(newProduct);
+        newProduct.productId = newProduct._id;
+        await newProduct.save();
+        res.send(newProduct);
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("error");
+    }
+};
+
+
+const deleteFavorit = async (req, res) => {
+    const { productId } = req.params;
+    console.log("Attempting to delete productId:", productId); 
+    try {
+        const isDeleted = await Favorite.findByIdAndDelete(productId);
+        if (isDeleted) {
+            console.log("Product deleted successfully", isDeleted); 
+            return res.send({ message: 'Product deleted' });
+        } else {
+            console.log("Product not found", productId); 
+            return res.status(401).send({ message: 'Product not found' });
+        }
+    } catch (error) {
+        console.log("Error in deleting product", error);
+        return res.status(400).send("Error");
+    }
+}
+
+
+
+
+
+module.exports = {addFavorite,deleteFavorit,getFavorite}
