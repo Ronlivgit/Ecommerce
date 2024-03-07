04070d898a58b12524b1a74a553e349f784ea227
yizak223 <139069183+yizak223@users.noreply.github.com>
2024-03-07 16:28:32 +0200

    paypal start to working i think

--- FrontEnd/src/pages/wishlist/Wishlist.jsx
+++ FrontEnd/src/pages/wishlist/Wishlist.jsx
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react";
-
+import PayPalPayment from "../../components/PayPalPayment";
 
 
 const faveURL = 'http://localhost:3000/api/favorite'
 export default function Wishlist() {
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetchProducts();
-  }, []);
 
+  const [products, setProducts] = useState([]);
+  const [loading, setIsLoading] = useState(false)
+  const [error, setError] = useState('')
 
   const fetchProducts = async () => {
- 
     try {
       const response = await fetch(faveURL);
       if (!response.ok) {
@@ -26,32 +24,38 @@ export default function Wishlist() {
       setIsLoading(false);
     }
   };
-  
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
 
- 
-      <div style={{ display: "flex", flexWrap: "wrap", padding: "10rem" }}>
-        {products.map((prod, index) => (
-          <div key={index} className="product-card">
-            <h2>{prod.name}</h2>
-            <p>{prod.description}</p>
-            <p>${prod.price}</p>
-            {prod.image && (
-              <img
-                src={prod.image}
-                alt={prod.name}
-                style={{ width: "100px", height: "100px" }}
-              />
-            )}
-            <br />
-    
-            <button onClick={() => addToBasket(prod._id)}>Add to Basket</button>
-          </div>
-        ))}
-      </div>
-    
-  
-);
-    
-  
+
+    <div style={{ display: "flex", flexWrap: "wrap", padding: "10rem" }}>
+      {products.map((prod, index) => (
+        <div key={index} className="product-card">
+          <h2>{prod.name}</h2>
+          <p>{prod.description}</p>
+          <p>${prod.price}</p>
+          {prod.image && (
+            <img
+              src={prod.image}
+              alt={prod.name}
+              style={{ width: "100px", height: "100px" }}
+            />
+          )}
+          <br />
+
+          <button onClick={() => addToBasket(prod._id)}>Add to Basket</button>
+          {/* yizak worked down here doesnt finish */}
+          <PayPalPayment />
+        </div>
+      ))}
+    </div>
+
+
+  );
+
+
 }
